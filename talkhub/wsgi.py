8a76829800e575170892faf1b8d1e75d7ee57592
ck1999 <ck199999@rambler.ru>
2019-10-05 18:53:17 +0300

    Update wsgi.py

--- talkhub/wsgi.py
+++ talkhub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkhub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mousesite.settings')
 
 application = get_wsgi_application()
