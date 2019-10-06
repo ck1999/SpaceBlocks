9232fbda9354c3ef794c52fa74775d9bfe54bdf6
ck1999 <ck199999@rambler.ru>
2019-10-06 18:46:15 +0400

    Update views.py

--- mouse/views.py
+++ mouse/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import mousesite
 from django.http import HttpResponse
-
+from .models import *
+from forms import *
 
 def index(request):
     return render(request, 'index.html')
@@ -9,4 +10,12 @@ def index(request):
 
 def blocks(request):
     """list of blocks"""
+    return render(request, 'blocks.html')
+
+def add_block(request):
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            AddBlock = form.save(commit=False)
+            AddBlock.msg = '123'
     return render(request, 'blocks.html')
