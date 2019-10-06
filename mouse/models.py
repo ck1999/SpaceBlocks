fe34559305b134032c00bd8ea703aa0f63733bd3
ck1999 <ck199999@rambler.ru>
2019-10-06 18:22:29 +0400

    Update models.py

--- mouse/models.py
+++ mouse/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Block(models.Model){
+    hash =  models.CharField(max_length=15)
+    nonce = models.IntegerField()
+    date = models.DateTimeField()
+
+    def get_hash(self):
+        return self.hash
+
+    def get_nonce(self):
+        return self.nonce
+
+    def get_date(self):
+        return self.date
+}
