d36ac24674a67eb9175386f4d3c1fb3099372535
ck1999 <ck199999@rambler.ru>
2019-10-06 16:32:59 +0400

    Added new page

--- mouse/views.py
+++ mouse/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 # Create your views here.
+
+def blocks_page(request):
+    """list of blocks"""
+    return render(request, 'blocks.html', context)
