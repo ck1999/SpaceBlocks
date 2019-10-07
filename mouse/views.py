fadfce8cfb6f95a60c60d53c5b84cbdb00b9dfd1
Yasuhiro <arseniy2003bulgakov@gmail.com>
2019-10-08 00:14:50 +0300

    QUICK FIX

--- mouse/views.py
+++ mouse/views.py
@@ -23,6 +23,6 @@ def add_block(request):
             latest_id = Block.objects.latest('id').id
             item = Block(nonce = int(nonce_a), time = datetime.datetime.now(), msg=msg_a)
             item.hash = item.calc_hash(Block.objects.get(id = latest_id).hash)
-        if item.validate():
-            item.save()
+            if item.validate():
+                item.save()
     return render(request, 'add.html', {'form': f})  
