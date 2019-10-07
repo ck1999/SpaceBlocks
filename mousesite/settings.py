01522f99f61664cb2d365bb0fa6f7dbf1ca7486d
ck1999 <ck199999@rambler.ru>
2019-10-07 23:03:46 +0400

    Update settings.py

--- mousesite/settings.py
+++ mousesite/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-import dj_database_url
+"""import dj_database_url
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)"""
