150a4a24c01996ba0f2d64a08019daee28b60bee
Kirill Sayed <sayed.kirill@gmail.com>
2021-08-09 10:29:58 +0300

    Added database switch for testing on CI

--- mousesite/settings.py
+++ mousesite/settings.py
@@ -79,17 +79,24 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd326tej2hd74cb',
-        'USER': 'tmrmceytmtzrff',
-        'PASSWORD': 'f5f7e7efe33d7844072326e11b5930fc0e1e97838f02fbfdf186ec1ec35e100a',
-        'HOST': 'ec2-54-217-234-157.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
+if os.environ.get('TEST') == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd326tej2hd74cb',
+            'USER': 'tmrmceytmtzrff',
+            'PASSWORD': 'f5f7e7efe33d7844072326e11b5930fc0e1e97838f02fbfdf186ec1ec35e100a',
+            'HOST': 'ec2-54-217-234-157.eu-west-1.compute.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
