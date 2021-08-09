abb28a206e439265e82328a428b49f8eedc1ac66
Kirill Sayed <sayed.kirill@gmail.com>
2021-08-09 10:36:16 +0300

    Added python3.6 for testing

--- mousesite/settings.py
+++ mousesite/settings.py
@@ -79,7 +79,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if os.environ.get('TEST') == 'True':
+if os.environ.get('TEST') == 'true':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
