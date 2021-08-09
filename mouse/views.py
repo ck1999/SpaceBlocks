d06ce1e2ebeb0b25ea2358dc3fe47d939a201f60
Kirill Sayed <sayed.kirill@gmail.com>
2021-08-09 09:53:58 +0300

    Refactored views

--- mouse/views.py
+++ mouse/views.py
@@ -1,40 +1,64 @@
 import datetime
 
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
+from django.views import View
 
 from mouse.forms import AddBlock, SignUpForm
 from mouse.models import Block as BlockModel
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(View):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.template_name = 'index.html'
+
+    def get(self, request: HttpRequest) -> render:
+        return render(request, self.template_name)
 
 
-def blocks(request):
-    blocks = BlockModel.objects.all()
-    context = {'blocks': blocks}
-    return render(request, 'blocks.html', context)
+class BlockView(View):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-@login_required
-def add_block(request):
-    f = AddBlock(request.POST)
-    if request.method == 'POST':
-        if f.is_valid():
-            nonce_a = f.data['nonce']
-            msg_a = f.data['msg']
+        self.template_name_list = 'blocks.html'
+        self.template_name_add_block = 'add.html'
+        self.context = {}
+
+    def get(self, request: HttpRequest) -> render:
+        blocks = BlockModel.objects.all()
+        self.context = {'blocks': blocks}
+
+        return render(request, self.template_name_list, self.context)
+
+    def post(self, request: HttpRequest) -> render:
+        form = AddBlock(request.POST)
+        if form.is_valid():
+            nonce_a = form.data['nonce']
+            msg_a = form.data['msg']
             latest_id = BlockModel.objects.latest('id').id
             item = BlockModel(nonce=int(nonce_a), time=datetime.datetime.now(), msg=msg_a)
             item.hash = item.calc_hash(BlockModel.objects.get(id=latest_id).hash)
             if item.validate():
                 item.save()
-    return render(request, 'add.html', {'form': f})
+        return render(request, self.template_name_add_block, {'form': form})
 
 
-def signup(request):
-    if request.method == 'POST':
+class SignupView(View):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.template_name = 'registration/signup.html'
+
+    def get(self, request: HttpRequest) -> render:
+        return render(request, self.template_name, {'form': SignUpForm()})
+
+    def post(self, request: HttpRequest):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
@@ -43,14 +67,17 @@ def signup(request):
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('/')
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        else:
+            return self.get(request)
 
 
-@login_required
-def account(request):
-    context = {
-        'user': request.user,
-    }
-    return render(request, 'registration/profile.html', context)
+class ProfileView(View):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.template_name = 'registration/profile.html'
+
+    def get(self, request: HttpRequest) -> render:
+        context = {'user': request.user}
+        return render(request, self.template_name, context)
